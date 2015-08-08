961b539a4fe09255f7c277dcb6952ab2dc3b2890
Degiac <gnarvash.3@gmail.com>
2015-08-08 23:23:46 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# TicTacToeJS
+
+It's a very simple Tic Tac Toe game made as a Zipline project for FreeCodeCamp.
+
+It uses the MiniMax algorithm to decide which actions to take. It uses memoization to make the execution faster.
---
+++ base.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+  var game;
+  
+  $('.choice').click(function () {
+    game = new TicTacToe($(this).html());
+    
+    for(var i = 0; i < 9; i++)
+      $('#' + i).html("");  
+    
+    $('.choice-group').fadeOut(500, function() {
+      $('.board').fadeIn(500);
+    });
+    
+    // X always begins. If the player has chose O,
+    // then the AI has to make its first move
+    if(game.agent === 'X')
+      $('#' + game.getAgentAction()).html(game.agent);
+  });
+  
+  $('.btn-board').click(function () {
+    var cell = Number(this.id);
+    
+    if(game.setPlayerAction(cell)) {
+      $(this).html(game.player);
+      
+      if(game.isFull()) { // if the board is full, it's a tie
+        $('.board').fadeOut(500, function () {
+          $('.tie').fadeIn(500);
+        });
+      } else {
+        $('#' + game.getAgentAction()).html(game.agent);
+        if(game.whoIsWinning()) { // check if the AI has won
+          $('.board').fadeOut(500, function () {
+            $('.lose').fadeIn(500);
+          });          
+        } else if(game.isFull()) {
+          $('.board').fadeOut(500, function () {
+            $('.tie').fadeIn(500);
+          });
+        }
+      }
+    }
+  });
+  
+  $('.replay').click(function () {
+    $(this).parent().fadeOut(500, function () {
+      $('.choice-group').fadeIn(500);
+    })
+  })
+});
